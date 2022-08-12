5c751de467638036de4022abeee5b75e3221843a
Alfonso Chavarro <alfonsocortezz25@gmail.com>
2022-08-12 18:34:26 -0500

    Added styles to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import PokemonLogo from '../../assets/pokeredux_logo.svg'
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <nav>
+        <img src={PokemonLogo} alt="" />
+      </nav>
+      <nav>
+        By{" "}
+        <a
+          href="https://github.com/JoseCortezz25"
+          target="_blank"
+          implies
+          rel="noopener"
+        >
+          Alfonso Chavarro
+        </a>
+      </nav>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
